8dfbee8b73d67921daf0575634cc8ea2e3acc869
jawhnypoh <po.johnny@gmail.com>
2018-01-16 15:30:41 -0800

    Little things update, trying to get icon in location button

--- app/src/main/java/com/example/poj/atmosphere/MainActivity.java
+++ app/src/main/java/com/example/poj/atmosphere/MainActivity.java
@@ -18,6 +18,9 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getActionBar().setDisplayShowTitleEnabled(false);
+
     }
 
     public void askPermission() {
